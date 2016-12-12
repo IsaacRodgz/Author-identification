efa3c9577d508455bd797df364530813ad43da1d
IsaacRodgz <chack.rb@gmail.com>
2016-12-12 07:44:20 -0600

    Confidence prediction

--- text2vect.py
+++ text2vect.py
@@ -2,6 +2,8 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import linear_model
 import numpy as np
+import random
+import math
 
 def get_new_author_vector(n_author, vectorizer):
 	w_dir = os.getcwd()
@@ -34,7 +36,7 @@ def get_authors_matrix():
 	vectorizer = CountVectorizer(min_df=2, stop_words='english', ngram_range=(1,3), analyzer = 'char')
 	vtf = vectorizer.fit_transform(corpus).transpose()
 
-	return vtf, vectorizer
+	return vtf.toarray(), vectorizer
 
 def predict_author(X, new_text_vect, author_label):
 
@@ -43,7 +45,6 @@ def predict_author(X, new_text_vect, author_label):
 	lasso_reg.fit(X, new_text_vect)
 
 	#Calculate distances and predict author
-	X = X.toarray()
 	w_predicted = lasso_reg.coef_
 	num_authors = X.shape[1]
 	residuals = []
@@ -53,17 +54,51 @@ def predict_author(X, new_text_vect, author_label):
 		y_hat = np.dot(X,w)
 		residuals.append((np.linalg.norm(y_hat-new_text_vect), i))
 
-	return "\nAuthor: {0}. Predicted: {1}".format(author_label, min(residuals)[1])
+	return author_label, min(residuals)[1]
+
+def cut_array(X):
+	num_rows = math.ceil(X.shape[0]*0.3)
+	selected_rows = random.sample(range(X.shape[0]), num_rows)
+	X_temp = np.delete(X, (selected_rows), axis=0)
+	return X_temp
+
+def calculate_score(author_predicted):
+	count = 0
+	for value1, value2 in author_predicted:
+		if value1 == value2:
+			count+=1
+	return count/len(author_predicted)
 
 def predict():
-	res = []
+	#Predicted authors
+	prediction = []
 	X, vectorizer = get_authors_matrix()
 	for i in range(7):
 		new_author_vector = get_new_author_vector(i, vectorizer)
-		res.append(predict_author(X, new_author_vector, i))
-	for output in res:
-		print(output)
+		prediction.append(predict_author(X, new_author_vector, i))
+
+	#Confidence values
+	res = [[] for i in range(7)]
+	X_aux, vectorizer = get_authors_matrix()
+	for j in range(10):
+		print('\ninteration: '+str(j))
+		X = cut_array(X_aux)
+		for i in range(7):
+			new_author_vector = get_new_author_vector(i, vectorizer)
+			new_author_vector = cut_array(new_author_vector)
+			res[i].append(predict_author(X, new_author_vector, i))
+
+	scores = []
+	for author_predicted in res:
+		scores.append(calculate_score(author_predicted))
+
+	print("\nResult\n")
+	for p, s in zip(prediction, scores):
+		print("Author: {0}, Predicted: {1}, Confidence: {2}\n".format(p[0], p[1], s))
 
 if __name__ == "__main__":
 	#vectorize(sys.argv[1])
 	predict()
+
+	#octpy
+	#5532777633
